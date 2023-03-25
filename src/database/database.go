3718cb37f2395028954766051016a81d6cde70f0
Matheus Lima <matheuslima.bsantos@gmail.com>
2023-03-24 22:28:12 -0300

    String conexao remota adicionada - hostinger

--- src/database/database.go
+++ src/database/database.go
@@ -3,27 +3,37 @@ package database
 import (
 	"api/src/config"
 	"database/sql"
+	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var production = false
+var production = true
 var stringConnec string
 
 func Connect() (*sql.DB, error) {
 
 	if production {
 		// conexao remota
+		fmt.Println("Usando conexão remota agora...")
 		stringConnec = os.Getenv("STRING_REMOTE")
 	} else {
 		// conexao local
 		stringConnec = config.StringConexaoBanco
 	}
 
+	fmt.Println("-----------------------------STRING CONEXAO-----------------------------------")
+	fmt.Println(stringConnec)
+	fmt.Println("-------------------------------------------------------------------------------")
+
 	db, err := sql.Open("mysql", stringConnec)
 	if err != nil {
 		return nil, err
+	}
+
+	if production {
+		fmt.Println("A conexao ao banco de dados remota esta pronta!")
 	}
 
 	if err := db.Ping(); err != nil {
